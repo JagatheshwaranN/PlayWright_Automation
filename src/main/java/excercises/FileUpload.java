7870bbb290eb7a8f2f4af686f5029702b111474b
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-01-03 20:31:35 +0530

    File Upload using FileChooser

--- src/main/java/excercises/FileUpload.java
+++ src/main/java/excercises/FileUpload.java
@@ -7,40 +7,64 @@ import java.nio.file.Paths;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.FileChooser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.FilePayload;
 
 public class FileUpload {
 
-	public static void main(String[] args) throws InterruptedException {
+	static Playwright playwright;
 
-		Playwright playwright = Playwright.create();
+	public static Page before() {
+
+		playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 		BrowserContext browserContext = browser.newContext();
 		Page page;
 		page = browserContext.newPage();
-		
+		return page;
+	}
 
+	public static void fileChooser() throws InterruptedException {
+		Page page = before();
+		page.navigate("https://davidwalsh.name/demo/multiple-file-upload.php");
+		FileChooser fileChooser = page.waitForFileChooser(() -> page.locator("#filesToUpload").click());
+		fileChooser.setFiles(Paths.get("orangeHRMlogin.json"));
+		Thread.sleep(2000);
+		fileChooser.setFiles(new Path[0]);
+		fileChooser.setFiles(new Path[] { Paths.get("orangeHRMlogin.json"), Paths.get("trace-view.zip") });
+		Thread.sleep(2000);
+		fileChooser.setFiles(new Path[0]);
+		fileChooser.setFiles(new FilePayload("demo.txt", "text/plain",
+				"This is playwright filechooser demo".getBytes(StandardCharsets.UTF_8)));
+		System.out.println(fileChooser.element());
+		System.out.println(fileChooser.isMultiple());
+		System.out.println(fileChooser.page().content());
+		page.close();
+		playwright.close();
+	}
+
+	public static void setInputFiles() throws InterruptedException {
+		Page page = before();
 		page.navigate("https://davidwalsh.name/demo/multiple-file-upload.php");
 		page.setInputFiles("input#filesToUpload", Paths.get("orangeHRMlogin.json"));
 		Thread.sleep(2000);
 		page.setInputFiles("input#filesToUpload", new Path[0]);
-		Thread.sleep(3000);
 		page.setInputFiles("input#filesToUpload",
 				new Path[] { Paths.get("orangeHRMlogin.json"), Paths.get("trace-view.zip") });
 		Thread.sleep(2000);
 		page.setInputFiles("input#filesToUpload", new Path[0]);
-		Thread.sleep(3000);
 		page.setInputFiles("input#filesToUpload",
 				new FilePayload("demo.txt", "text/plain", "This is playwright demo".getBytes(StandardCharsets.UTF_8)));
-		Thread.sleep(2000);
-		page.setInputFiles("input#filesToUpload", new Path[0]);
-
-		// page.navigate("https://cgi-lib.berkeley.edu/ex/fup.html");
-
-		browser.close();
+		page.close();
 		playwright.close();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		fileChooser();
+		setInputFiles();
 
 	}
 
