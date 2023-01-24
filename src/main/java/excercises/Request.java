5bd65a79d4d13954f6e66dfd64dd81b49be45802
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-01-24 20:15:25 +0530

    Request Scenarios Updated

--- src/main/java/excercises/Request.java
+++ src/main/java/excercises/Request.java
@@ -1,10 +1,13 @@
 package excercises;
 
+import java.util.List;
+
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.HttpHeader;
 import com.microsoft.playwright.options.LoadState;
 
 public class Request {
@@ -15,14 +18,22 @@ public class Request {
 		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 		BrowserContext browserContext = browser.newContext();
 		Page page = browserContext.newPage();
-	//	page.onRequest(request -> System.out.println(">>" + request.headers()));
-		System.out.println("**************");
-		page.onRequest(request -> System.out.println(request.headerValue("content-type")));
-		page.onRequest(request -> System.out.println(request.headerValue("charset")));
+		page.onRequest(request -> System.out.println(">>" + request.headers()));
+//		page.onRequest(request -> System.out.println("Content : " + request.headerValue("content-type")));
+//		page.onRequest(request -> System.out.println("User Agent : " + request.headerValue("user-agent")));
+		System.out.println("***************");
+		page.onRequest(request -> {
+			List<HttpHeader> arr = request.headersArray();		
+			for(HttpHeader a : arr) {
+				System.out.println(a);
+			}
+		});
+		page.onRequestFailed(request -> {
+			System.out.println(request.url() + " " + request.failure());
+		});
 		page.navigate("https://www.google.com/");
 		page.waitForLoadState(LoadState.NETWORKIDLE);
-		
-	
+
 	}
 
 }
