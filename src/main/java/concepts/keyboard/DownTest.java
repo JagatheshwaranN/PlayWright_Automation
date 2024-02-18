e86c7fc0f3594cddaf6428f59d9d8259a7a9429a
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-02-18 09:42:39 +0530

    Playwright Concepts - Keyboard Scenarios - Down, Up, Press, InsertText and Type Added.

---
+++ src/main/java/concepts/keyboard/DownTest.java
@@ -0,0 +1,67 @@
+package concepts.keyboard;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.Test;
+
+
+public class DownTest {
+
+    @Test
+    public void testKeyboardDown(){
+        // Initialize playwright variable to null
+        Playwright playwright = null;
+
+        // Declaring variable for a browser type
+        BrowserType browserType;
+
+        // Initialize browser variable to null
+        Browser browser = null;
+
+        try {
+            // Create Playwright instance
+            playwright = Playwright.create();
+
+            // Use Chromium as the browser type
+            browserType = playwright.chromium();
+
+            // Launch the browser
+            browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+            // Create a new isolated browser context
+            BrowserContext browserContext = browser.newContext();
+
+            // Create a new page within the context
+            Page page = browserContext.newPage();
+
+            // Navigate to the demo page
+            page.navigate("file:///D:/Environment_Collection/Intellij_Env/Playwright_Concepts/support/list.html");
+
+            // Store the locator (e.g., CSS selector, XPath) for the input field as String
+            String inputLocator = "//input[@title='pass']";
+
+            // Focus on the input field
+            page.focus(inputLocator);
+
+            // Simulate pressing the 'a' key
+            page.keyboard().down("a");
+
+            // Simulate pressing the 'b' key while 'a' is still held down
+            page.keyboard().down("b");
+
+        } catch (Exception ex) {
+            // Print the exception stack trace for debugging
+            ex.printStackTrace();
+        } finally {
+            // Close the browser if it's running
+            if (browser != null) {
+                browser.close();
+            }
+
+            // Close the Playwright object to release resources
+            if (playwright != null) {
+                playwright.close();
+            }
+        }
+    }
+
+}
